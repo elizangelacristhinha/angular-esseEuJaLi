fb3173cd1c454dd71f5c16ef36ec54cc899312c4
Unknown <elizangelacristhina@gmail.com>
2023-06-05 12:11:18 -0300

    Atividades 04 e 08.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import * as M from 'materialize-css';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('mobile') sideNav?:ElementRef;
+
   title = 'angular-esseEuJaLi';
+  ngAfterViewInit():void{
+    let $sideNav = $ ('#mobile-demo') ;
+    M.Sidenav.init($sideNav);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDeLivrosComponent } from './lista-de-livros/lista-de-livros.component';
 import { LoginComponent } from './lista-de-livros/login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaDeLivrosComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
