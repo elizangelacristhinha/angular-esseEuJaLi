5c521f0a58e44435fe96a28b243acc6a7c886903
Unknown <elizangelacristhina@gmail.com>
2023-06-20 08:36:01 -0300

    Atividade 11.
    Atividade 11.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { ListaDeLivrosComponent } from './lista-de-livros/lista-de-livros.compon
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
-
+import { UserComponent } from './user/user.component';
+import { AuthenticationGuard } from './util/authentication.guard';
+import { CadastrarUsuarioComponent } from './cadastrar-usuario/cadastrar-usuario.component';
+import { FormsModule } from '@angular/forms';
 const routes: Routes = [
   { path: '', component: TelaLoginComponent },
   { path: 'listaDeLivros', component: ListaDeLivrosComponent },
-  { path: 'visualizarLivro', component: VisualizarLivroComponent }];
+  { path: 'visualizarLivro', component: VisualizarLivroComponent },
+  { path: 'cadastrarUsuario', component: CadastrarUsuarioComponent },];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CpfPipe } from './pipes/cpf.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDeLivrosComponent } from './lista-de-livros/lista-de-livros.component';
@@ -8,20 +9,28 @@ import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
 import { VisualizarLivroComponent } from './visualizar-livro/visualizar-livro.component';
+import { ExponentialStrengthPipe } from './pipes/exponential-strength.pipe';
+import { CadastrarUsuarioComponent } from './cadastrar-usuario/cadastrar-usuario.component';
 //import { ModelComponent } from './model/model.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
+    ExponentialStrengthPipe,
     AppComponent,
     ListaDeLivrosComponent,
     MenuComponent,
     FooterComponent,
+    CpfPipe,
+    UserComponent,
     TelaLoginComponent,
-    VisualizarLivroComponent//,
+    VisualizarLivroComponent,
+    CadastrarUsuarioComponent//,
     //ModelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
